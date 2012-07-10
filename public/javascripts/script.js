cc592a77621a35bf157a6563177c80504a79abdc
Francisco Javier Henseleit Palma <dezmaeth@gmail.com>
2012-07-10 19:53:51 -0400

    first commit

---
+++ public/javascripts/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var GroupSquare = GroupSquare = function() {
+
+	var __construct = function() { 
+		if (navigator.geolocation) { 
+			//navigator.geolocation.getPosition();
+		}
+	}	
+
+	__construct();
+}
+
+GroupSquare = new GroupSquare;
